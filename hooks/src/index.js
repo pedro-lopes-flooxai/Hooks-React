98e1eb119c2e4477969294b060802ec995690823
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-17 12:10:44 -0300

    add: useContext #1

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -6,4 +6,7 @@ import App from './views/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+<App />
+ 
+);
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import { useContext } from 'react';
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/DataContext'
+
 const UseContext = (props) => {
+
+    const {state, setState} = useContext(DataContext)
+
+    const context = useContext(DataContext)
+
+    function addNumber(delta) {
+        setState({
+            ...context.state,
+            number: context.state.number + delta
+        })
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
-        </div>
+                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"/>
+      
+       <div className='center'>
+            <span className='text'>{context.state.text}</span>
+            <span className='text'>{context.state.number}</span>
+      
+            <div>
+                <button className='btn'
+                    onClick={() => addNumber(-1)}>-1</button>
+                <button className='btn'
+                onClick={() => addNumber(+1)}>+1</button>
+            </div>
+       </div>
+     </div>
     )
 }
 
