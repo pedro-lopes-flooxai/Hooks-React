9339f541dad3eb1c1da0ee1be3d72fe4730b3345
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-17 10:34:01 -0300

    add: useRef #2

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,13 +2,27 @@ import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = function(s1,s2) {
+    return [...s1].map((e,i) => `${e}${s2[i] || ""}`).join("")
+}
+
 const UseRef = props => {
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+
     const count = useRef(0)
+    const myIput1 = useRef(null)
+    const myIput2 = useRef(null)
 
     useEffect(function(){
         count.current = count.current + 1
+        myIput2.current.focus()
     }, [value1])
+
+    useEffect(function(){
+        count.current++
+        myIput1.current.focus()
+    }, [value2])
 
     return (
         <div className="UseRef">
@@ -20,13 +34,22 @@ const UseRef = props => {
             <div className="center">
                  <div>
                     <span className="text">Valor: </span>
-                    <span className="text">{value1} [</span>
+                    <span className="text">{merge(value1,value2)} [</span>
                     <span className="text red">{count.current}</span>
                     <span className="text">]</span>
-                </div>
+            </div>
 
         <input type="text" className="input"
+        ref={myIput1}
             value={value1} onChange={e => setValue1(e.target.value)}/>
+          </div>
+
+          
+          <SectionTitle title="Exercico #02" />
+    <div className='center'>
+          <input type="text" className="input"
+          ref={myIput2}
+            value={value2} onChange={e => setValue2(e.target.value)}/>
           </div>
      </div>
     )
